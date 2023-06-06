257179f67639a226835669933071a29fc6e6080e
Chima Chike <chimachike1@gmail.com>
2023-06-06 16:08:25 -0700

    12-fizzbuzz.py

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 def fizzbuzz():
     for number in range(1, 101):
-        if (number % 3) == 0:
-            number = 'Fizz'
-        elif (number % 5) == 0:
-            number = 'Buzz'
-        elif (number % 3 and number % 5) == 0:
+        if number % 3 == 0 and num % 5 == 0:
             number = 'FizzBuzz'
+        elif number % 5 == 0:
+            number = 'Buzz'
+        elif number % 3 == 0:
+            number = 'Fizz'
         else:
             return (number)
     print("{:d}".format(number), end=' ')
