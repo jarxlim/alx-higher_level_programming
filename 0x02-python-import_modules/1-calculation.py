834ad7c75ee65c91f56d66656445dbd39600fc56
Chima Chike <chimachike1@gmail.com>
2023-06-08 08:28:28 -0700

    1-calculation.py

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    from calculator_1 import *
+
+    a = 10
+    b = 5
+
+    print("{0:d} + {1:d} = {2:d}".format(a, b, add(a,b)))
+    print("{0:d} - {1:d} = {2:d}".format(a, b, add(a,b)))
+    print("{0:d} * {1:d} = {2:d}".format(a, b, add(a,b)))
+    print("{0:d} / {1:d} = {2:d}".format(a, b, add(a,b)))
