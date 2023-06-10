33004da0152ecd79792ac2bb129259dc048f0960
Chima Chike <chimachike1@gmail.com>
2023-06-10 09:53:10 -0700

    5-no_c.py

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    for i in my_string:
-        if i == chr(67) or i == chr(99):
-            return my_list.remove(i)
+    for char in my_string:
+        if char == chr(67) or i == chr(99):
+            return my_string.remove(char)
