07be31b48a1cf9fb998d0d43f4fc25ff93cb5d6f
Chima Chike <chimachike1@gmail.com>
2023-06-10 10:11:45 -0700

    5-no_c.py

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -4,4 +4,4 @@ def no_c(my_string):
     for char in my_string:
         if char != chr(67) and char != chr(99):
             new_string += char
-            return new_string
+    return new_string
