45598182309c2a71f04676710875085b94ba8123
Chima Chike <chimachike1@gmail.com>
2023-06-11 11:14:54 -0700

    10-divisible_by_2.py

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
+    result = []
     for i in my_list:
-        if i % 2 == 0
-        return True
-    else:
-        return False
+        if i % 2 == 0:
+            return True
+        else:
+            return False
+    return result
