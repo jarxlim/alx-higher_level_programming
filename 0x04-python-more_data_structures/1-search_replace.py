4a475afc4e4296ab2426be85fb1cc5617da399a6
Chima Chike <chimachike1@gmail.com>
2023-06-14 12:21:15 -0700

    1-search_replace.py

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    for i in range(len(my_list)):
-        if my_list[i] == search:
-            my_list[i] = replace
-    return my_list
+    newlist = my_list.copy()
+    for i in range(len(newlist)):
+        if newlist[i] == search:
+            newlist[i] = replace
+    return newlist
