23daabc444b4104b4cfc972c7c414994ad8e4461
Chima Chike <chimachike1@gmail.com>
2023-06-14 14:52:10 -0700

    10-best_score.py

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,4 +2,4 @@
 def best_score(a_dictionary):
     if a_dictionary is None or a_dictionary == {}:
         return None
-    return max(a_dictionary, key=lambda key:a_dictionary[key])
+    return max(a_dictionary, key=lambda key: a_dictionary[key])
