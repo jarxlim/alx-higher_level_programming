d8bb954eac0d7c2a2771c0987444f7970b66614e
Chima Chike <chimachike1@gmail.com>
2023-06-14 11:51:12 -0700

    0-square_matrix_simple.py

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    return [list(map(lambda x: x**2, i)) for i in matrix]
+    result = []
+    result.append(list(map(lambda x: x**2, i)) for i in matrix)
+    return (result)
