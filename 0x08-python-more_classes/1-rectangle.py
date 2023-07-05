db14a130b7e9d82a26671ee978f5d686bfbfd269
chimachike1@gmail.com <chimachike1@gmail.com>
2023-07-05 05:21:17 -0700

    add task 2

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -18,12 +18,12 @@ class Rectangle:
     @width.setter
     def width(self, value):
         '''sets the width atttribute'''
-        if isinstance(value, int):
-            self.__width = value
-            if value < 0:
-                raise TypeError('width must be >= 0')
-        else:
+        if not isinstance(value, int):
             raise TypeError('width must be an integer')
+        if value < 0:
+            raise TypeError('width must be >= 0')
+        else:
+            self.__heigth = value
 
     @property
     def heigth(self):
