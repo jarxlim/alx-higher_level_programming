56901599b8c38bb9fe34a6e7b6be19d4caf345a7
chimachike1@gmail.com <chimachike1@gmail.com>
2023-07-05 05:52:18 -0700

    add task 2

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 
-'''A module that contains a class that defines a rectangle'''
+"""A module that contains a class that defines a rectangle"""
 
 
 class Rectangle:
-    '''
-    an empty class Rectangle that defines a rectangle:
-    '''
+    """an empty class Rectangle that defines a rectangle:"""
     pass
