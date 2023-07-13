8d92263933f28c08692d28bd56ccab4c19fc0e42
chimachike1@gmail.com <chimachike1@gmail.com>
2023-07-13 11:53:51 -0700

    1-write_file.py

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 '''write_file function documentation'''
+
+
 def write_file(filename="", text=""):
     '''function that writes a string to a text file (UTF8)'''
     with open(filename, "w", encoding='utf-8') as myFile:
