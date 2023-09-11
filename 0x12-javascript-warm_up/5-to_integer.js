d07c560cf9b4fd9ed319c6ad5d52eef05bdbef01
chimachike1@gmail.com <chimachike1@gmail.com>
2023-09-11 15:15:34 -0700

    done

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+const arg = process.argv;
+const num = parseInt(arg[2], 10);
+
+if (isNaN(num)) {
+  cosole.log('Not a number');
+} else {
+  console.log(`My number: ${num}`);
+}
