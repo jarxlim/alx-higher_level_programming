8ac85612a4adc03307f6be9b272c11d68145eced
chimachike1@gmail.com <chimachike1@gmail.com>
2023-09-12 15:03:52 -0700

    done

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,19 +1,11 @@
 #!/usr/bin/node
-if (process.argv.length < 3) {
-  console.log('Factorial of NaN is 1');
-} else {
-  const number = parseInt(process.argv[2]);
-  if (!isNaN(number)) {
-    function factorial (n) {
-      if (n <= 1) {
-        return 1;
-      } else {
-        return n * factorial(n - 1);
-      }
-    }
-    const result = factorial(number);
-    console.log(`Factorial of ${number} is ${result}`);
+
+const x = process.argv[2];
+function factorial (x) {
+  if (isNaN(x) || x === 1) {
+    return (1);
   } else {
-    console.log('Factorial of NaN is 1');
+    return (x * factorial(x - 1));
   }
 }
+console.log(factorial(parseInt(x)));
