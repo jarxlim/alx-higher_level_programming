b9be367b4f8a75dd792f86a0ac76791e2deaf97c
chimachike1@gmail.com <chimachike1@gmail.com>
2023-09-12 14:51:55 -0700

    done

--- 0x13-javascript_objects_scopes_closures/9-logme.js
+++ 0x13-javascript_objects_scopes_closures/9-logme.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 
-const countx = 0;
+let countx = 0;
 
 exports.logMe = function countx (item) {
-  console.log(`${countx}: ${item}`);
+  console.log(countx + ': ' + item);
   countx += 1;
 };
