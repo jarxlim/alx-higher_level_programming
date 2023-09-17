d445865b831fe8f7a366f0f727a2187919dbb2f8
Chike Chima <chimachike1@gmail.com>
2023-09-17 13:52:32 -0700

    10-model_state_my_get.py

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,26 +1,31 @@
 #!/usr/bin/python3
 """
-script that lists all State objects that contain the letter a
+script that prints the State object with the name passed as argument
 from the database hbtn_0e_6_usa
 """
 
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 from sys import argv
-from model_state import Base, State
-from sqlalchemy.orm import Session
-from sqlalchemy import (create_engine)
+from model_state import State, Base
 
 if __name__ == "__main__":
 
     user = argv[1]
     password = argv[2]
     database = argv[3]
+    name = argv[4]
 
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format
                            (user, password, database), pool_pre_ping=True)
     Base.metadata.create_all(engine)
 
     session = Session(engine)
-    for state in session.query(State).order_by(State.id.asc()).all():
-        if 'a' in state.name:
-            print("{}: {}".format(state.id, state.name))
+    state = session.query(State).filter(State.name == name).first()
+
+    if state is not None:
+        print("{}".format(state.id))
+    else:
+        print("Not found")
+
     session.close()
