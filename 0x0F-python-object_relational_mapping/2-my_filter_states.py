3f15d01adbd3e3f5da9ff899b43d00acba9e04a6
Chike Chima <chimachike1@gmail.com>
2023-09-17 11:06:09 -0700

    2-my_filter_states.py

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -4,7 +4,7 @@ script that takes in an argument and displays all values in the states
 table of hbtn_0e_0_usa where name matches the argument.
 """
 
-import MySQLdb as db
+import MySQLdb
 from sys import argv
 
 if __name__ == '__main__':
@@ -17,7 +17,6 @@ if __name__ == '__main__':
 
     db = MySQLdb.connect(host='localhost', port=3306, user=user,
                          passwd=password, db=database)
-
     cur = db.cursor()
     cur.execute(query)
 
