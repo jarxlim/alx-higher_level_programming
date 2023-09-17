b7739597ea0c37a4002546d58743c36f90023f3c
Chike Chima <chimachike1@gmail.com>
2023-09-17 14:12:37 -0700

    12-model_state_update_id_2.py

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,27 @@
+#!/usr/bin/pythoni3
+"""
+script that changes the name of a State object from the database hbtn_0e_6_usa
+"""
+
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine
+from sys import argv
+from model_state import Base, State
+
+if __name__ == "__main__":
+
+    user = argv[1]
+    password = argv[2]
+    database = argv[3]
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format
+                           (user, password, database), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    session = Session(engine)
+    newState = session.query(State).filter_by(id=2).first()
+    newState.name = 'New Mexico'
+    session.add(newState)
+    session.commit()
+
+    session.close()
