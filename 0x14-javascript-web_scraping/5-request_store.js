e2107e3448484a6ca479845503ff4f90513fa42f
root <chimachike1@gmail.com>
2023-10-24 14:42:12 -0700

    done

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -5,7 +5,6 @@ const request = require('request');
 const url = process.argv[2];
 const filename = process.argv[3];
 
-
 request(url, function (err, response, body) {
   if (err) {
     console.log(err);
